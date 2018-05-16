90c5a08cde79f65b7917ac97d35e08db4b3f593d
Vanderlei Sbaraini Amancio <vanderlei@meucambio.com.br>
2018-05-16 16:17:14 -0300

    Added analytics tag

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @include('_layouts.partials.gtag')
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
